0e2e78d3ae458ab8d16a94b5020da1e5f36f6ecd
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2025-05-10 00:37:35 +0300

    upload the 3rd lab work

---
+++ lab3_raytracing/Form1.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Windows.Forms;
+
+namespace Raytracing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        View view = new View();
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            view.Draw();
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            view.initShaders();
+            view.initBufferData();
+        }
+    }
+}
---
+++ lab3_raytracing/View.cs
@@ -0,0 +1,78 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace Raytracing
+{
+    class View
+    {
+        private int BasicProgramID;
+        private int BasicVertexShader;
+        private int BasicFragmentShader;
+        private int vbo_position;
+        private int vao;
+        private int attribute_vpos;
+        Vector3[] vertdata;
+        private int uniform_aspect;
+        private double aspect;
+        private int uniform_pos;
+        private Vector3 campos;
+
+        public void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        public void initShaders()
+        {
+            BasicProgramID = GL.CreateProgram();
+            loadShader("..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            GL.LinkProgram(BasicProgramID);
+
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+        }
+
+        public void initBufferData()
+        {
+            vertdata = new Vector3[]
+            {
+                new Vector3(-1f, -1f, 0f),
+                new Vector3(1f, -1f, 0f),
+                new Vector3(1f, 1f, 0f),
+                new Vector3(-1f, 1f, 0f),
+            };
+
+            // Создаем и заполняем VBO
+            GL.GenBuffers(1, out vbo_position);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo_position);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length * Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+
+            // Указываем как читать атрибуты
+            GL.VertexAttribPointer(attribute_vpos, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(attribute_vpos);
+            GL.Uniform3(uniform_pos, campos);
+            GL.Uniform1(uniform_aspect, aspect);
+            GL.UseProgram(BasicProgramID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+        public void Draw()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+            GL.DisableVertexAttribArray(attribute_vpos);
+        }
+    }
+}
